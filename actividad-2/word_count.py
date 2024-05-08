429d1e4b2c73a7774b1414c56b6c421b5f11ffae
Marco Antonio Contreras <70834807+EniDev911@users.noreply.github.com>
2024-05-07 22:12:40 -0400

    Update word_count.py add whitespace

--- actividad-2/word_count.py
+++ actividad-2/word_count.py
@@ -4,10 +4,10 @@ CUR_DIR = os.path.dirname(__file__)
 
 with open(f"{CUR_DIR}/lorem_ipsum.txt", "r") as file:
 	contenido = file.read()
-	palabras_separadas = contenido.split()
+	palabras_separadas = contenido.split(" ")
       
 palabras_unicas = set(palabras_separadas)
-caracteres_unicos = set("".join(palabras_separadas))
+caracteres_unicos = set(" ".join(palabras_separadas))
 
 print("El número de caracteres distintos es:", len(caracteres_unicos))
 print("El número de palabras distintas es:", len(palabras_unicas))
