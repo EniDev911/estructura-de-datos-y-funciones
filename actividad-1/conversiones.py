5c0e4c07eac8171335da825d019dcbe9bdfd515e
enidev911 <enidev911@gmail.com>
2024-05-15 01:17:02 -0400

    up

--- actividad-1/conversiones.py
+++ actividad-1/conversiones.py
@@ -1,13 +1,20 @@
 import sys
 
-conversiones = {
-    "Soles": sys.argv[1],
-    "Pesos Argentinos": sys.argv[2],
-    "Dólares": sys.argv[3]
-}
+if len(sys.argv) >= 5:
 
-pesos_chileno = int(sys.argv[4])
+    conversiones = {
+        "Soles": sys.argv[1],
+        "Pesos Argentinos": sys.argv[2],
+        "Dólares": sys.argv[3]
+    }
 
-print("Los", pesos_chileno, "equivalen a:")
-for k,v in conversiones.items():
-    print(pesos_chileno * float(v), k)
+    pesos_chileno = int(sys.argv[4])
+
+    print("Los", pesos_chileno, "equivalen a:")
+    for k,v in conversiones.items():
+        print(pesos_chileno * float(v), k)
+
+else:
+	print("Debes pasarme como mínimo 4 argumentos desde la línea de comando. En este orden:")
+	print(*["Sol Peruano", "Peso argentino", "Dólar americano", "Peso Chileno"], sep="\n")
+	print("$python conversiones.py 0.0046 0.093 0.0013 10000")
